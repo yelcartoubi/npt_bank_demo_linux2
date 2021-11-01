a3ac20b338e6c6b63b19726e2d79866047cd51b2
wison <Wison.Ling@newlandpayment.com>
2021-11-01 15:46:45 +0800

    transfer file format : unix to windows

--- inc/exception.h
+++
@@ -1,7 +0,0 @@
-#ifndef _EXCEPTION_H_
-#define _EXCEPTION_H_
-
-extern void InitException(void);
-
-#endif
-
--- inc/tr.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TR_H_
-#define _TR_H_
-
-extern char *tr(const char *str);
-extern void DestroyCustomizedFont(void);
-#endif
-
